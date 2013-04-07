0d5757eba79804f249283e9ad8a994ee0fc1125b
Matteo Collina <matteo.collina@gmail.com>
2013-04-07 17:11:40 +0200

    Added colors package.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 var package = require("./package");
+var colors = require('colors');
 
-console.log(package.name, "starting up");
-console.log("version", package.version);
+var output = "";
+output += package.name.red;
+output += "@";
+output += package.version.yellow;
+output += " starting up".green;
+
+console.log(output);
