8aaae8bd3c14bb1832fbbba82fc14534951dff0d
Matteo Collina <matteo.collina@gmail.com>
2013-04-07 18:31:46 +0200

    Added basic server.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 
 var package = require("./package");
 var colors = require('colors');
+var express = require('express');
+var app = express();
 
-var output = "";
-output += package.name.red;
-output += "@";
-output += package.version.yellow;
-output += " starting up".green;
+app.use(express.logger());
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.cookieSession({
+  secret: "a very long and RANDOM secret: 42" 
+}));
+app.use(express.static("public"));
 
-console.log(output);
+app.get("/", function (req, res) {
+  res.send("Hello world!");
+});
+
+app.listen(3000, function (err) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  var output = "";
+  output += package.name.red;
+  output += "@";
+  output += package.version.yellow;
+  output += " started up on port 3000".green;
+
+  console.log(output);
+});
